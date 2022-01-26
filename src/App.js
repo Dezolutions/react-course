50bd8db1d801330ecfcb8ee3ae3dd16cd48d0780
Dezolutions <mars92851@gmail.com>
2022-01-27 01:51:11 +0200

    jsx commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './App.css';
+import Car from './Car/Car'
+import React from 'react';
 
 const App = () => {
-  return <div></div>;
+  const divStyle = {
+    textAlign: 'center'
+  }
+  return (
+    <div style={divStyle}>
+      <h1>Hello world!</h1>
+
+      <Car name={'Ford'} year={2018}>
+        <p style={{color: 'blue'}}>COLOR</p>
+      </Car>
+      <Car name="Audi" year={2016}>
+        <p style={{color: 'red'}}>COLOR</p>
+      </Car>
+      <Car name={'Mazda'} year={2010} />
+    </div>
+  );;
 };
 
 export default App;
-
