852136ddf629dde8d2c22d3be07f3da0ec6e427e
Dezolutions <mars92851@gmail.com>
2022-02-14 17:34:57 +0200

    creating tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React from 'react'
+import Character from './Character'
 
-const App = () => {
-  return <div></div>;
-};
+const App = ({side}) => {
+  if (!side) {
+    side = 'light'
+  }
 
-export default App;
+  const characters = [
+    {name: 'Люк Скайвокер', side: 'light'},
+    {name: 'Йода', side: 'light'},
+    {name: 'Обиван Кеноби', side: 'light'},
+    {name: 'Палпатин', side: 'dark'},
+    {name: 'Дарт Вэйдер', side: 'dark'}
+  ]
 
+  const filteredChars = characters.filter(char => char.side === side)
+
+  return (
+    <ul>
+      {filteredChars.map((char, index) => (
+        <Character key={char.name + index} name={char.name} side={char.side} />
+      ))}
+    </ul>
+  )
+}
+
+export default App
